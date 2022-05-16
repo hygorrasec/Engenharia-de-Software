d43f2e5288a5640e18731565898d6671fded285c
hygorrasec <coehmaneh@gmail.com>
2022-05-15 23:31:46 -0300

    Spring React - Delta AVA Movie

---
+++ Spring React/deltaavamovie/backend/src/test/java/com/deltaava/deltaavamovie/DeltaavamovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deltaava.deltaavamovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeltaavamovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
